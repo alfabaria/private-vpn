abe33ad11965b4c9cd239fbf9a909c0db0d556e8
Alfa Ibrahim Daulat baria <47588662+alfabaria@users.noreply.github.com>
2022-08-31 11:42:46 +0700

    Update install-proxy.sh
    update proxy install script

--- install-proxy.sh
+++ install-proxy.sh
@@ -1,22 +1,30 @@
 #!/bin/bash
-# <UDF name="squid_user" Label="Proxy Username" />
-# <UDF name="squid_password" Label="Proxy Password" />
-# Squid Proxy Server
-# Author: admin@radwebhosting.com
-# Web: https://radwebhosting.com
 
+SQUID_USER=$1
+SQUID_PASSWORD=$2
+IP=$(curl -s https://ipinfo.io/ip)
 
 /usr/bin/apt update
-/usr/bin/apt -y install apache2-utils squid3
+/usr/bin/apt -y install squid apache2-utils
 
 /usr/bin/htpasswd -b -c /etc/squid/passwd $SQUID_USER $SQUID_PASSWORD
 
 /bin/rm -f /etc/squid/squid.conf
 /usr/bin/touch /etc/squid/blacklist.acl
-/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/Rad-Web-Hosting/squid-proxy/master/squid.conf
+/usr/bin/wget --no-check-certificate -O /etc/squid/squid.conf https://raw.githubusercontent.com/alfabaria/private-vpn/main/squid.conf.txt
 
 /sbin/iptables -I INPUT -p tcp --dport 3128 -j ACCEPT
 /sbin/iptables-save
 
 service squid restart
 update-rc.d squid defaults
+
+echo ""
+echo "Looks like the script has finished successfully."
+echo ""
+echo "Proxy Address : $IP"
+echo "Proxy User : $VPN_USER"
+echo "Proxy Password : $VPN_PASSWORD"
+echo ""
+echo "Set your proxy server in your browser to http://$IP:3128"
+echo ""
